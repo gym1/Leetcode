b718268962a7c777b9ad03f50228126d0c48046b
gym1 <lawrenceyiming@gmail.com>
2018-07-09 22:07:46 -0400

    Create Reverse_Vowels_of_a_String.c

---
+++ Reverse_Vowels_of_a_String.c
@@ -0,0 +1,34 @@
+
+void reverse(int *a, char *s, int beg, int end)
+{
+    while(beg<end)
+    {
+        if(a[s[beg]]==1&&a[s[end]]==1)
+        {
+            s[beg]=s[beg]^s[end];
+            s[end]=s[beg]^s[end];
+            s[beg]=s[beg]^s[end];
+            beg++;
+            end--;
+        }
+        while(a[s[beg]]!=1&&beg<end)
+        {
+            beg++;
+        }
+        while(a[s[end]]!=1&&beg<end)
+        {
+            end--;
+        }
+    }       
+}
+char* reverseVowels(char* s) {
+    int i=0;
+    int j=strlen(s)-1;
+    if(j<1)
+    {return s;}
+    int a[123]={0};
+    a['a']=1;a['e']=1;a['i']=1;a['o']=1;a['u']=1;
+    a['A']=1;a['E']=1;a['I']=1;a['O']=1;a['U']=1;
+    reverse(a, s, i, j);
+    return s;
+}
